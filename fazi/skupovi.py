a2c73d99f3a41059e4880eabad58bf6cc68d7f52
Dusan <dusanslankamenac8@gmail.com>
2026-04-21 21:17:02 +0200

    Azurirani skupovi
    Skupovi sad imaju lepsa imena koja ih bolje opisuju.

--- fazi/skupovi.py
+++ fazi/skupovi.py
@@ -6,13 +6,13 @@ x_broj_linkova  = np.arange(0, 21, 1)
 x_caps_procenat = np.arange(0, 101, 1)
 x_spam_score    = np.arange(0, 101, 1)
 
-kljucne_retke    = fuzz.trapmf(x_kljucne_reci, [0,  0,  2,  5])
-kljucne_umerene  = fuzz.trimf (x_kljucne_reci, [3,  7, 12])
-kljucne_ucestale = fuzz.trapmf(x_kljucne_reci, [10, 14, 20, 20])
+kljucne_zanemarljive = fuzz.trapmf(x_kljucne_reci, [0,  0,  2,  5])
+kljucne_zastupljene  = fuzz.trimf (x_kljucne_reci, [3,  7, 12])
+kljucne_dominantne   = fuzz.trapmf(x_kljucne_reci, [10, 14, 20, 20])
 
-linkovi_oskudni   = fuzz.trapmf(x_broj_linkova, [0,  0,  1,  3])
+linkovi_minimalni = fuzz.trapmf(x_broj_linkova, [0,  0,  1,  3])
 linkovi_umereni   = fuzz.trimf (x_broj_linkova, [2,  5,  9])
-linkovi_preobilni = fuzz.trapmf(x_broj_linkova, [8, 12, 20, 20])
+linkovi_brojni    = fuzz.trapmf(x_broj_linkova, [8, 12, 20, 20])
 
 caps_uobicajen = fuzz.trapmf(x_caps_procenat, [0,  0,  10, 25])
 caps_poviseni  = fuzz.trimf (x_caps_procenat, [15, 35, 55])
