123b28e0b9de585d25210229b1298ef8ded33ee7
Dusan <dusanslankamenac8@gmail.com>
2026-04-23 17:50:40 +0200

    Promenjen caps i score ulaz

--- fazi/skupovi.py
+++ fazi/skupovi.py
@@ -15,17 +15,17 @@ linkovi_minimalni = fuzz.trapmf(x_broj_linkova, [0, 0, 1, 3])
 linkovi_umereni   = fuzz.trimf (x_broj_linkova, [1, 3, 5])
 linkovi_brojni    = fuzz.trapmf(x_broj_linkova, [4, 6, 10, 10])
 
-caps_uobicajen = fuzz.trapmf(x_caps_procenat, [0, 0, 6, 12])
-caps_poviseni  = fuzz.trimf (x_caps_procenat, [10, 20, 35])
+caps_uobicajen = fuzz.trapmf(x_caps_procenat, [0, 0, 1, 10])
+caps_poviseni  = fuzz.trimf (x_caps_procenat, [7, 18, 35])
 caps_agresivan = fuzz.trapmf(x_caps_procenat, [25, 45, 100, 100])
 
 interpunkcija_retka     = fuzz.trapmf(x_interpunkcija, [0,  0,  2,  5])
 interpunkcija_umerena   = fuzz.trimf (x_interpunkcija, [3, 10, 20])
 interpunkcija_agresivna = fuzz.trapmf(x_interpunkcija, [15, 25, 100, 100])
 
-score_legitiman = fuzz.trapmf(x_spam_score, [0, 0, 20, 35])
-score_sumnjiv   = fuzz.trimf (x_spam_score, [30, 50, 70])
-score_spam      = fuzz.trapmf(x_spam_score, [60, 75, 100, 100])
+score_legitiman = fuzz.trapmf(x_spam_score, [0, 0, 10, 25])
+score_sumnjiv   = fuzz.trimf (x_spam_score, [15, 35, 55])
+score_spam      = fuzz.trapmf(x_spam_score, [45, 65, 100, 100])
 
 
 def stepen_pripadnosti(x_universe, mf, vrednost: float) -> float:
